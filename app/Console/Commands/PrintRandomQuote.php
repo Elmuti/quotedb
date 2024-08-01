cc2868de58665ef75564950e575758f5268ac745
Elmuti <elmukelmuz@gmail.com>
2024-08-02 01:10:57 +0300

    fix

---
+++ app/Console/Commands/PrintRandomQuote.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Models\Quote;
+use App\Models\User;
+use Illuminate\Console\Command;
+
+class PrintRandomQuote extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'quote:print {--email=}';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        $user = User::where("email", $this->option("email"))->firstOrFail();
+        $quote = $user->quotes()->inRandomOrder()->first();
+        $this->alert($quote->quote);
+    }
+}
