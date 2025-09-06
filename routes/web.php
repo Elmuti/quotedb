84acab508c0c8786698432535a74fc3844ac1f4f
Elmuti <elmukelmuz@gmail.com>
2025-09-06 19:45:26 +0300

    fix logout

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,8 @@ use App\Http\Controllers\HomeController;
 use App\Http\Controllers\QuotesController;
 use App\Http\Controllers\RandomIzaroController;
 use App\Http\Controllers\RandomQuoteController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [HomeController::class, 'index'])->name('home');
@@ -16,6 +18,7 @@ Route::get('/izaro', [RandomIzaroController::class, 'index'])->name('izaro');
 
 Route::get('/logout', function (Request $request) {
     Auth::logout();
-
+    $request->session()->invalidate();
+    $request->session()->regenerateToken();
     return redirect(route('home'));
 });
