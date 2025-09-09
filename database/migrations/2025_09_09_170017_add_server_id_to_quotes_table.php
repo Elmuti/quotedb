8cb13f80d5bcc01dabe3237b858874b29ae28ec1
Mirasii <meatheat_100mean@hotmail.co.uk>
2025-09-09 18:30:07 +0100

    remove da after author thingy

--- database/migrations/2025_09_09_170017_add_server_id_to_quotes_table.php
+++ database/migrations/2025_09_09_170017_add_server_id_to_quotes_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('quotes', function (Blueprint $table) {
-            $table->unsignedBigInteger('server_id')->nullable()->index()->after('author');
+            $table->unsignedBigInteger('server_id')->nullable()->index();
         });
     }
 
